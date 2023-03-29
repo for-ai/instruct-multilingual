30e797d22ea906b527591fb2583cbc4b85345bdd
Kowasaki <weiyinko@gmail.com>
2023-03-29 02:29:14 +0000

    revamped translate_datasets to use inference api

--- instructmultilingual/translate_datasets.py
+++ instructmultilingual/translate_datasets.py
@@ -1,111 +1,105 @@
 import os
+import requests
+import time
 
-from datasets import load_dataset
-from flores_200 import lang_name_to_code
-from optimum.onnxruntime import ORTModelForSeq2SeqLM
+from datasets import load_dataset, DatasetDict
+from instructmultilingual.flores_200 import lang_name_to_code
+from multiprocessing import cpu_count
 from pathlib import Path
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-from typing import List
+from typing import Dict, List, Tuple, Union
 
 
-    
-def translate_dataset(
-        dataset_name: str,
-        splits: List[str],
-        translate_cols:List[str],
-        output_dir: str,
-        target_languages: List[str],
-        source_language: str = "English",
-        checkpoint: str = "facebook/nllb-200-3.3B",
-        device: str = "cuda:0",
-        printout: bool = False,
-    ) -> None:
+def translate(url, source_language, target_language, texts):
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "source_language": source_language,
+        "target_language": target_language,
+        "texts": texts,
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()["translated_texts"]
 
-    datasets = []
+
+def tokenization(
+    example,
+    url,
+    source_lang_code,
+    target_lang_code,
+    keys_to_be_translated=["dialogue", "summary"],
+):
+    for key in keys_to_be_translated:
+        example[key] = translate(url, source_lang_code, target_lang_code, example[key])
+    return example
+
+
+def translate_dataset_via_api(
+    dataset: DatasetDict,
+    dataset_name: str,
+    splits: List[str],
+    translate_keys: List[str],
+    target_language: str,
+    url: str = "http://localhost:8000/translate",
+    output_dir: str = "/home/weiyi/instruct-multilingual/datasets",
+    source_language: str = "English",
+    checkpoint: str = "facebook/nllb-200-3.3B",
+    num_proc: int = cpu_count(),
+) -> None:
+    """_summary_
+
+    Args:
+        dataset (DatasetDict): _description_
+        dataset_name (str): _description_
+        splits (List[str]): _description_
+        translate_keys (List[str]): _description_
+        target_language (str): _description_
+        url (_type_, optional): _description_. Defaults to "http://localhost:8000/translate".
+        output_dir (str, optional): _description_. Defaults to "/home/weiyi/instruct-multilingual/datasets".
+        source_language (str, optional): _description_. Defaults to "English".
+        checkpoint (str, optional): _description_. Defaults to "facebook/nllb-200-3.3B".
+        num_proc (int, optional): _description_. Defaults to cpu_count().
+    """
+
+    source_language_code = lang_name_to_code[source_language]
+    target_language_code = lang_name_to_code[target_language]
+
+    start_time = time.time()
+
     for split in splits:
-        datasets.append(load_dataset(dataset_name, split=split))
+        split_time = time.time()
+        ds = dataset[split]
+        print(f"[{split}] {len(ds)=}")
+        ds = ds.map(
+            lambda x: tokenization(
+                x,
+                url=url,
+                source_lang_code=source_language_code,
+                target_lang_code=target_language_code,
+                keys_to_be_translated=translate_keys,
+            ),
+            batched=True,
+            num_proc=num_proc,
+        )
+        print(f"[{split}] One example translated {ds[0]=}")
+        print(f"[{split}] took {time.time() - split_time:.4f} seconds")
 
-    if "onnx" in checkpoint:
-        model = ORTModelForSeq2SeqLM.from_pretrained(checkpoint)
-    else:
-        model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-    model.to(device)
+    print(f"Elapsed time: {elapsed_time:.4f} seconds")
 
-    for dataset in datasets:
-        for target_language in target_languages:
-            translated_dataset = dataset.to_pandas()
-
-            source_language_code = lang_name_to_code[source_language]
-            target_language_code = lang_name_to_code[target_language]
-
-            tokenizer = AutoTokenizer.from_pretrained(
-                checkpoint,
-                src_lang=source_language_code,
-                tgt_lang=target_language_code,
-            )
-            
-
-            for i in range(len(dataset)):
-                for col in translate_cols:
-                    text = translated_dataset.loc[i, col]
-                    
-                    inputs = tokenizer(text, return_tensors="pt").to(device)
-                    translated_tokens = model.generate(
-                        **inputs,
-                        forced_bos_token_id=tokenizer.lang_code_to_id[target_language_code],
-                        max_length=1024,
-                    )
-                    decoded = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
-                    translated_dataset.loc[i, f"{col}_{target_language}"] = decoded
-                    if printout:
-                        print(f"{i}| Original ({source_language}): {text}")
-                        print(f"{i}| Translated ({target_language}): {decoded}")
-
-            checkpoint_str = checkpoint.replace("/", "-") 
-            translated_dir = Path(os.path.join(output_dir, dataset_name, target_language_code))
-            translated_dir.mkdir(parents=True, exist_ok=True)
-            translated_dataset.to_csv(os.path.join(translated_dir, f"{dataset_name}_{split}_{target_language_code}_{checkpoint_str}.csv"), index=False)
-            
-    
-
-
-if __name__ == "__main__":
-    root = "/home/weiyi/instruct-multilingual/datasets"
-
-    # translate_dataset(
-    #     "rotten_tomatoes", 
-    #     ["validation"],
-    #     ["text"],
-    #     root, 
-    #     [
-    #         # "Egyptian Arabic",
-    #         # "Bengali",
-    #         # "Western Persian",
-    #         "Chinese (Traditional)",
-    #         "Portuguese",
-    #     ],
-    #     # checkpoint="facebook/nllb-200-distilled-600M",
-    #     checkpoint="facebook/nllb-200-3.3B",
-    #     # printout=True
-    # )
-
-    translate_dataset(
-        "samsum", 
-        ["validation"],
-        ["dialogue", "summary"],
-        root, 
-        [
-            # "Egyptian Arabic",
-            # "Bengali",
-            # "Western Persian",
-            # "Chinese (Traditional)",
-            "Chinese (Simplified)",
-            # "Portuguese",
-        ],
-        # checkpoint="facebook/nllb-200-distilled-600M",
-        # checkpoint="facebook/nllb-200-3.3B",
-        # checkpoint="onnx_facebook_nllb-200-3.3B/",
-        checkpoint="onnx_facebook_nllb-200-distilled-600M",
-        # printout=True
+    checkpoint_str = checkpoint.replace("/", "-")
+    translated_dir = Path(os.path.join(output_dir, dataset_name, target_language_code))
+    translated_dir.mkdir(parents=True, exist_ok=True)
+    ds.to_csv(
+        os.path.join(
+            translated_dir,
+            f"{dataset_name}_{split}_{target_language_code}_{checkpoint_str}.csv",
+        ),
+        index=False,
+    )
+    ds.to_json(
+        os.path.join(
+            translated_dir,
+            f"{dataset_name}_{split}_{target_language_code}_{checkpoint_str}.jsonl",
+        )
     )
