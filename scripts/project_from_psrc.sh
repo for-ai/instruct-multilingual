e0846ce8e603ed6164380eb99e7748d5d04860b2
M Saiful Bari <sbmaruf@gmail.com>
2023-04-19 04:10:49 +0800

    xp3 option example

--- scripts/project_from_psrc.sh
+++ scripts/project_from_psrc.sh
@@ -1,14 +1,30 @@
+# The native format with a lots of metadata
 DUMP_FOLDER='./raw'
 SRC_DATA_FOLDER=$DUMP_FOLDER/projection_from_psrc
 mkdir -p $SRC_DATA_FOLDER
 mkdir -p $SRC_DATA_FOLDER/cache
 
 python data/project_from_psrc.py \
---dataset-name-or-paths glue glue glue glue glue \
---dataset-configs cola sst2 mrpc qqp stsb \
---prompt-templates-configs None None None None None \
+--dataset-name-or-paths nq_open \
+--dataset-configs None \
+--prompt-templates-configs None \
 --cache-dir $SRC_DATA_FOLDER/cache \
 --output-dir $SRC_DATA_FOLDER \
 --highlight-variables \
 --add-source-metadata \
+--num-proc 16
+
+# The xP3 format
+DUMP_FOLDER='./raw'
+SRC_DATA_FOLDER=$DUMP_FOLDER/projection_from_psrc
+mkdir -p $SRC_DATA_FOLDER
+mkdir -p $SRC_DATA_FOLDER/cache
+
+python data/project_from_psrc.py \
+--dataset-name-or-paths nq_open \
+--dataset-configs None \
+--prompt-templates-configs None \
+--cache-dir $SRC_DATA_FOLDER/cache \
+--output-dir $SRC_DATA_FOLDER \
+--xp3-format \
 --num-proc 16
