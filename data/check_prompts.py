76a50d40daefa64fb7616cfa575b9498d83dfd68
M Saiful Bari <sbmaruf@gmail.com>
2023-04-23 02:48:43 +0800

    update jinja prompt loader

--- data/check_prompts.py
+++ data/check_prompts.py
@@ -1,8 +1,27 @@
 import os
 import csv
+import json
 import argparse
 import subprocess
+from promptsource.templates import Template
+from .data_stat import SERIES_A_DATASET_NAME_DICT
 
+def check(
+	json_example, 
+	template_name, 
+	jinja_template, 
+	template_reference=None, 
+	answer_choices=None
+):
+	json_example = json.loads(json_example)
+	template = Template(
+		template_name, 
+	 	jinja_template, 
+	  	template_reference, 
+	   	answer_choices=answer_choices
+	)
+	lm_io = template.apply(json_example, highlight_variables=False)
+	return lm_io
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -29,7 +48,6 @@ def main():
 		subprocess.check_output(f"mv {prompt_file_path} {prompt_file_path}.old", shell=True)
 		subprocess.check_output("curl -L https://docs.google.com/spreadsheets/d/10bCwOhM8zKNkqKi54gIvdwrR44YlWQFV9fpGm7acHv8/export?format=csv > ./data/prompts.csv", shell=True)
   
-
 	with open('data/prompts.csv', 'r') as csvfile:
 		csvreader = csv.reader(csvfile)
 		next(iter(csvreader))
