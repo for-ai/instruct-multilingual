32e473be578af436eb5147a495a77545a58a923d
M Saiful Bari <sbmaruf@gmail.com>
2023-04-30 21:10:01 +0800

    example script renamed

---
+++ scripts/project_from_promptsource.sh
@@ -0,0 +1,30 @@
+# The native format with a lots of metadata
+DUMP_FOLDER='./raw'
+SRC_DATA_FOLDER=$DUMP_FOLDER/project_from_promptsource
+mkdir -p $SRC_DATA_FOLDER
+mkdir -p $SRC_DATA_FOLDER/cache
+
+python data/project_from_promptsource.py \
+--dataset-name-or-paths nq_open \
+--dataset-configs None \
+--prompt-templates-configs None \
+--cache-dir $SRC_DATA_FOLDER/cache \
+--output-dir $SRC_DATA_FOLDER \
+--highlight-variables \
+--add-source-metadata \
+--num-proc 16
+
+# # The xP3 format
+# DUMP_FOLDER='./raw'
+# SRC_DATA_FOLDER=$DUMP_FOLDER/project_from_promptsource
+# mkdir -p $SRC_DATA_FOLDER
+# mkdir -p $SRC_DATA_FOLDER/cache
+
+# python data/project_from_promptsource.py \
+# --dataset-name-or-paths nq_open \
+# --dataset-configs None \
+# --prompt-templates-configs None \
+# --cache-dir $SRC_DATA_FOLDER/cache \
+# --output-dir $SRC_DATA_FOLDER \
+# --xp3-format \
+# --num-proc 16
